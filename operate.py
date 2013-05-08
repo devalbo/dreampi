ee9e01c3906c392886b6cc062aeacdc2d388ce1d
Albert Boehmler <ajboehmler@gmail.com>
2013-05-07 23:25:40 -0400

    Adding website

--- operate.py
+++ operate.py
@@ -1,4 +1,5 @@
 
+import os
 import time
 from optparse import OptionParser
 import dream_cheeky
@@ -7,6 +8,9 @@ parser = OptionParser()
 opts, args = parser.parse_args()
 
 turret = dream_cheeky.Turret(opts)
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+static_dir = os.path.abspath(os.path.join(current_dir, "static"))
 
 
 def move_n_seconds(move_fn, n_seconds=2.0):
@@ -18,80 +22,50 @@ def move_n_seconds(move_fn, n_seconds=2.0):
 
 
 if __name__ == "__main__":
-    from bottle import route, run
+    from bottle import route, run, static_file
 
-    @route('/hello')
-    def hello():
-        return "Hello World!"
+    @route('/')
+    def index():
+        return static_file("index.html", root=current_dir)
 
-    @route('/left')
+    @route('/static/<filepath:path>')
+    def server_static(filepath):
+        return static_file(filepath, root=static_dir)
+
+    @route('/left', method='POST')
     def left():
         print "Left"
         move_n_seconds(turret.launcher.turretLeft)
         return "Left"
 
-    @route('/right')
+    @route('/right', method='POST')
     def right():
         print "Right"
         move_n_seconds(turret.launcher.turretRight)
         return "Right"
 
-    @route('/down')
+    @route('/down', method='POST')
     def down():
         print "Down"
         move_n_seconds(turret.launcher.turretDown)
         return "Down"
 
-    @route('/up')
+    @route('/up', method='POST')
     def up():
         print "Up"
         move_n_seconds(turret.launcher.turretUp)
         return "Up"
 
-    @route('/stop')
+    @route('/stop', method='POST')
     def stop():
         print "Stop"
         turret.launcher.turretStop()
         return "Stop"
 
-    @route('/fire')
+    @route('/fire', method='POST')
     def fire():
         print "Fire"
         turret.launcher.turretFire()
         return "Fire"
 
     run(host='0.0.0.0', port=8080, debug=True)
-
-
-
-# def move_n_seconds(move_fn, n_seconds=8.0):
-#     print move_fn
-#     move_fn()
-#     time.sleep(n_seconds)
-#     turret.launcher.turretStop()
-#     print "done"
-
-
-# import sys
-# cmd = sys.argv[1]
-# print cmd.strip()
-
-# if cmd == "fire":
-#     turret.launcher.turretFire()
-
-# elif cmd == "right":
-#     move_n_seconds(turret.launcher.turretRight)
-
-# elif cmd == "left":
-#     move_n_seconds(turret.launcher.turretLeft)
-
-# elif cmd == "down":
-#     move_n_seconds(turret.launcher.turretDown)
-
-# elif cmd == "up":
-#     move_n_seconds(turret.launcher.turretUp)
-
-# elif cmd == "stop":
-#     turret.launcher.turretStop()
-
-
