26bc8908891164f5bea38a70c06bae8593c1a3ee
Albert Boehmler <ajboehmler@gmail.com>
2013-05-11 14:25:25 -0400

    Fixing Chrome/Ajax issues

--- flask_operate.py
+++ flask_operate.py
@@ -19,41 +19,58 @@ def move_n_seconds(move_fn, n_seconds=0.5):
     time.sleep(n_seconds)
     turret.launcher.turretStop()
     print "done"
+    
+from flask import make_response
+from functools import update_wrapper
+
+def nocache(f):
+    def new_func(*args, **kwargs):
+        resp = make_response(f(*args, **kwargs))
+        resp.cache_control.no_cache = True
+        return resp
+    return update_wrapper(new_func, f)
 
 
 from flask import Flask, redirect, send_file
 app = Flask(__name__)
 
 @app.route('/')
+@nocache
 def index():
     return send_file("index.html")
 
 @app.route('/left', methods=['POST'])
+@nocache
 def left():
     move_n_seconds(turret.launcher.turretLeft)
     return redirect('/')
 
 @app.route('/right', methods=['POST'])
+@nocache
 def right():
     move_n_seconds(turret.launcher.turretRight)
     return redirect('/')
 
 @app.route('/down', methods=['POST'])
+@nocache
 def down():
     move_n_seconds(turret.launcher.turretDown)
     return redirect('/')
 
 @app.route('/up', methods=['POST'])
+@nocache
 def up():
     move_n_seconds(turret.launcher.turretUp)
     return redirect('/')
 
 @app.route('/stop', methods=['POST'])
+@nocache
 def stop():
     turret.launcher.turretStop()
     return redirect('/')
 
 @app.route('/fire', methods=['POST'])
+@nocache
 def fire():
     turret.launcher.turretFire()
     return redirect('/')
