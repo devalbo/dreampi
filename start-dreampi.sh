1393c58fc05209e2d0c25c0041d4bd49b04dfd01
Albert Boehmler <ajboehmler@gmail.com>
2013-05-20 23:41:08 -0400

    Updating startup script instructions

--- start-dreampi.sh
+++ start-dreampi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# on Raspbian, put this file in /etc/init.d, then run update-rc.d start-dreampi.sh defaults
+# on Raspbian, put this file in /etc/init.d, then run 'chmod a+x /etc/init.d/start-dreampi.sh', then 'update-rc.d start-dreampi.sh defaults'
 
 cd /home/pi/Projects/dreampi
 sudo venv/bin/python flask_operate.py
